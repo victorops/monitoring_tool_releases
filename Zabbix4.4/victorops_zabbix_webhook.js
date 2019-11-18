37e431af616c962eb3334ed39c411c286076dcb9
cphillips2 <49244479+cphillips2@users.noreply.github.com>
2019-11-18 12:00:38 -0700

    scripts needed to integrate Zabbix4.4 via webhook

---
+++ Zabbix4.4/victorops_zabbix_webhook.js
@@ -0,0 +1,33 @@
+//Get all the alert variables
+var params = JSON.parse(value);
+//Zabbix.Log(1, "params created");
+var action_payload=JSON.parse(params.action_payload);
+//Zabbix.Log(1, "action payload created");
+
+//Declare curl request
+var req = new CurlHttpRequest();
+req.AddHeader('Content-Type: application/json');
+
+//Build JSON payload
+var fields = action_payload;
+//Zabbix.Log(1, "action payload written to fields");
+
+fields.VO_ORGANIZATION_KEY = params.VO_ORGANIZATION_KEY;
+fields.VO_ORGANIZATION_ID = params.VO_ORGANIZATION_ID;
+fields.VO_ROUTING_KEY = params.VO_ROUTING_KEY;
+fields.VO_ZABBIX_ALERT_TIME = Date.now();
+fields["ESC.HISTORY"] = params.VO_STATE_MESSAGE;
+
+//Zabbix.Log(1, "Payload: "+ JSON.stringify(fields));
+
+//Make the POST to the VO Zabbix Endpoint
+//https://alert.victorops.com/integrations/zabbix/20140212/alert
+var resp = req.Post('https://alert.victorops.com/integrations/zabbix/20140212/alert',
+	JSON.stringify(fields)
+);
+
+if (req.Status() != 200) {
+	throw 'Response code: '+req.Status();
+}
+
+return resp;
