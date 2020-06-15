4715274c5e4c4fe1a4b67131eeab4ec41baeecd6
cphillips2 <49244479+cphillips2@users.noreply.github.com>
2020-06-15 09:58:25 -0600

    Update victorops_zabbix_webhook.js
    Included regex parsing for JSON

--- VictorOps-Zabbix4.4/victorops_zabbix_webhook.js
+++ VictorOps-Zabbix4.4/victorops_zabbix_webhook.js
@@ -1,16 +1,18 @@
 //Get all the alert variables
 var params = JSON.parse(value);
 //Zabbix.Log(1, "params created");
-var action_payload=JSON.parse(params.action_payload);
-//Zabbix.Log(1, "action payload created");
+var action_payload=params.action_payload;
+//Zabbix.Log(1, "action payload created: " + action_payload);
 
-//Declare curl request
-var req = new CurlHttpRequest();
-req.AddHeader('Content-Type: application/json');
+//Use regex to check for invalid quotes and newline chars in the action field values
+//action_payload=action_payload.replace(/([ :])(\")([\w\s-/$]+)(\")([ }"\.])/g, "$1$3$5");
+//Zabbix.Log(1, "action payload regex 1: " + action_payload);
+//action_payload=action_payload.replace(/[\n\r]/g, " ");
+//Zabbix.Log(1, "action payload regex 2: " + action_payload);
 
 //Build JSON payload
-var fields = action_payload;
-//Zabbix.Log(1, "action payload written to fields");
+var fields = JSON.parse(action_payload);
+//Zabbix.Log(1, "FIELDS after PARSE: "+fields);
 
 //set fields for use in VictorOps endpoint mapping
 if (fields.MONITOR_NAME) {
@@ -18,23 +20,23 @@ fields.monitor_name = fields.MONITOR_NAME;
 fields.VO_ZABBIX_HOST = fields.MONITOR_NAME;
 delete fields.MONITOR_NAME;
 }
-
 fields.VO_ORGANIZATION_KEY = params.VO_ORGANIZATION_KEY;
 fields.VO_ORGANIZATION_ID = params.VO_ORGANIZATION_ID;
 fields.VO_ROUTING_KEY = params.VO_ROUTING_KEY;
 fields.VO_ZABBIX_ALERT_TIME = Date.now();
 fields["ESC.HISTORY"] = params.VO_STATE_MESSAGE;
 
-//Zabbix.Log(1, "Payload: "+ JSON.stringify(fields));
-
+//Declare curl request
+var req = new CurlHttpRequest();
+req.AddHeader('Content-Type: application/json');
 //Make the POST to the VO Zabbix Endpoint
 //https://alert.victorops.com/integrations/zabbix/20140212/alert
 var resp = req.Post('https://alert.victorops.com/integrations/zabbix/20140212/alert',
-	JSON.stringify(fields)
+JSON.stringify(fields)
 );
 
 if (req.Status() != 200) {
-	throw 'Response code: '+req.Status();
+throw 'Response code: '+req.Status();
 }
 
 return resp;
